e421bb63853c37024d645bfb7862d4f59619b7d9
Dat Nguyen <tomnguyen2111996@gmail.com>
2024-07-19 02:02:12 -0700

    da

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,30 +1,46 @@
 describe('Product Grid Tests', () => {
-  beforeEach(() => {
-      cy.visit('http://localhost:3000/product-test');
-  });
 
-  it('Page loads successfully', () => {
-      cy.get('.grid-layout').should('exist');
-  });
+    const mobileView = [375, 667];
+    const desktopView = [1024, 768];
 
-  it('Page contains 9 products', () => {
-      cy.get('.grid-tile').should('have.length', 9);
-  });
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/product-test');
+    });
 
-  it('Sale button works as expected', () => {
-      // Original prices
-      let originalPrices = [];
-      cy.get('.grid-layout .price').each(($price) => {
-          originalPrices.push(parseFloat($price.text().replace('$', '')));
-      });
+    it('Page loads successfully', () => {
+        cy.get('.sale-button').should('exist');
+    });
 
-      // Sale button
-      cy.get('.sale-button button').click();
 
-      // 50% prices
-      cy.get('.grid-tile .price').each(($price, index) => {
-          const newPrice = parseFloat($price.text().replace('$', ''));
-          expect(newPrice).to.equal((originalPrices[index] / 2));
-      });
-  });
+    it('Desktop view loads succesfully', () => {
+        cy.viewport(desktopView[0], desktopView[1]);
+        cy.get('.grid-layout-desktop')
+        .should('be.visible')
+    });
+
+    it('Mobile view loads succesfully', () => {
+        cy.viewport(mobileView[0], mobileView[1]);
+        cy.get('.grid-layout-mobile').should('be.visible');
+    });
+
+    it('Page contains 9 products', () => {
+        cy.get('.grid-tile').should('have.length', 9);
+    });
+
+    it('Sale button works as expected', () => {
+        // Original prices
+        let originalPrices = [];
+        cy.get('.grid-tile .price').each(($price) => {
+            originalPrices.push(parseFloat($price.text().replace('$', '')));
+        });
+
+        // Sale button
+        cy.get('.sale-button button').click();
+
+        // 50% prices
+        cy.get('.grid-tile .price').each(($price, index) => {
+            const newPrice = parseFloat($price.text().replace('$', ''));
+            expect(newPrice).to.equal((originalPrices[index] / 2));
+        });
+    });
 });
